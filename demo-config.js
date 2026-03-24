faeaeb5adf131486414f79262fabc8381bc75e5d
amirrchik22 <amirrchik22@gmail.com>
2026-03-25 00:51:18 +0300

    Delete demo-config.js

--- demo-config.js
+++
@@ -1,4 +0,0 @@
-window.DEMO_CONFIG = {
-  apiBaseUrl: "https://script.google.com/macros/s/AKfycbxTxg-TmcZiU2ghE6Vgnidp0A6VxSdGWM5JurcR5EmvWw0mX5Sp3yvOGu1A0nePIyJH/exec",
-  requestTimeoutMs: 15000
-};
